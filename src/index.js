e0eae0b349d1ef17b14da290bbb54bb8adfdcae0
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-28 19:05:17 +0800

    Control flowwith v-if and v-else

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ const app = Vue.createApp({
   template: `
     <button v-on:click="increment">Increment</button>
     <p>{{ count }}</p>
+    <div v-if="isEven()">Even</div>
+    <div v-else>Odd</div>
   `,
 
   methods: {
     increment() {
       this.count += 1
+    },
+    isEven() {
+      return (this.count % 2) === 0
     }
   },
 
