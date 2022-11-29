673c294dd295a085360cee4224780028d23bd2a0
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-29 23:16:12 +0800

    Child-Parent communication with events

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import * as Vue from 'vue/dist/vue.esm-bundler.js'
 
-
 const Hello = {
   props: ['greeting'],
   template: `<p>{{ greeting }}</p>`
@@ -9,16 +8,19 @@ const Hello = {
 const Number = {
   props: ['number'],
   template: `
-    <div :class="getClass(number)">
+    <button :class="getClass(number)"
+      @click="click">
       {{ number }}
-    </div>`,
+    </button>`,
   methods: {
     getClass(number) {
       return this.isEven(number) ? 'blue' : 'red'
     },
-
     isEven(number) {
       return number % 2 === 0
+    },
+    click() {
+      this.$emit('chosen', this.number)
     }
   }
 }
@@ -29,50 +31,30 @@ const app = Vue.createApp({
     Number
   },
   template: `
-    <Hello :greeting="'Halo'"/>
-    <Hello greeting="Hello"/>
-    <Hello greeting="Hi"/>
-    
-    <button v-on:click="increment">Increment</button>
-    <p>{{ count }}</p>
     <Number  v-for="number in numbers"
-      :number="number" />
-    <input type="text" v-model="value"/>
-    <br>
-    <input type="radio" value="a" v-model="value"/>
-    <input type="radio" value="b" v-model="value"/>
-    <br>
-    <input type="checkbox" value="a" v-model="value"/>
-    <input type="checkbox" value="b" v-model="value"/>
-    <br>
-    {{ value }}
-    <br>
-    <div class="red">
-      {{ error }}
-    </div>`,
+      :number="number" 
+      @chosen="addNumber"/>
+    <hr>
+    <Number  v-for="number in numberHistory"
+      :number="number" 
+      @chosen="addNumber"/>`,
 
   methods: {
-    increment() {
-      this.count += 1
-    },
+    addNumber(number) {
+      this.numberHistory.push(number)
+    }
   },
 
   computed: {
     evenList() {
       return this.numbers.filter(num => this.isEven(num))
     },
-    error() {
-      if(this.value.length < 5) {
-        return 'Must be greater than 5'
-      }
-    }
   },
 
   data() {
     return {
-      count: 0,
-      value: ['a'],
       numbers: [1,2,3,4,5,6,7,8,9,10],
+      numberHistory: []
     }
   },
 })
