6c46666c09ca1458424b9ee7df853cb393fe612b
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-12-01 21:33:21 +0800

    Reactivity with ref

--- composition/App.vue
+++ composition/App.vue
@@ -1,19 +1,35 @@
 <template>
+  <input v-model="msg" />
   <div>{{ msg }}</div>
+  <hr>
+  <button @click="increment">{{ count }}</button>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   setup() {
-    const msg = 'Hello Composition!'
+    const msg = ref('Hello Composition!')
+    const count = ref(0)
+    const increment = () => {
+      console.log(count)
+      count.value++
+    }
 
     return {
-      msg
+      msg,
+      count,
+      increment
     }
   }
 }
 </script>
 
 <style scoped>
-
+button{
+  height: 200px;
+  width: 200px;
+  font-size: 40px;
+}
 </style>
