a5d721700914bb8bdfd68418905a6b42af275197
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-12-01 21:43:19 +0800

    Reactivity for complex value

--- composition/App.vue
+++ composition/App.vue
@@ -3,15 +3,28 @@
   <div>{{ msg }}</div>
   <hr>
   <button @click="increment">{{ count }}</button>
+  <button @click="increase('a')">{{ numbers.a }}</button>
+  <button @click="increase('b')">{{ numbers.b }}</button>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
 export default {
   setup() {
+    // Ref => number, string (primitive)
     const msg = ref('Hello Composition!')
     const count = ref(0)
+    // Reactive => {} (Object)
+    const numbers = reactive({
+      a: 0,
+      b: 0
+    })
+
+    const increase = (index) => {
+      numbers[index]++
+    }
+
     const increment = () => {
       console.log(count)
       count.value++
@@ -20,7 +33,9 @@ export default {
     return {
       msg,
       count,
-      increment
+      numbers,
+      increment,
+      increase,
     }
   }
 }
@@ -28,8 +43,8 @@ export default {
 
 <style scoped>
 button{
-  height: 200px;
-  width: 200px;
+  height: 100px;
+  width: 100px;
   font-size: 40px;
 }
 </style>
