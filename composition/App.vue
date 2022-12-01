72dfb07254fa191decb0817dec732deeb78bd310
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-12-01 18:31:11 +0800

    Hello composition API

---
+++ composition/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const msg = 'Hello Composition!'
+
+    return {
+      msg
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
