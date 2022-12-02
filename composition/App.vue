ae746c570bad16cd67eac67ce2f9e51bdfe9bc55
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-12-02 19:08:42 +0800

    The useNumbers composable

--- composition/App.vue
+++ composition/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <input v-model="msg" />
-  <div>{{ msg }}</div>
-  <hr>
-  <button @click="increment">{{ count }}</button>
   <button @click="increase('a')">{{ numbers.a }}</button>
   <button @click="increase('b')">{{ numbers.b }}</button>
   <button @click="c++">{{ c }}</button>
@@ -14,68 +10,11 @@
 </template>
 
 <script>
-import { ref, reactive, computed, watch, watchEffect } from "vue";
+import { useNumbers } from "./useNumbers.js";
 
 export default {
   setup() {
-    // Ref => number, string (primitive)
-    const msg = ref('Hello Composition!')
-    const count = ref(0)
-    const c = ref(0)
-    const d = ref(0)
-    const history  = ref([])
-    // Reactive => {} (Object)
-    const numbers = reactive({
-      a: 1,
-      b: 2
-    })
-
-    const increase = (index) => {
-      numbers[index]++
-    }
-
-    const increment = () => {
-      console.log(count)
-      count.value++
-    }
-
-    const total = computed(() => numbers.a + numbers.b + count.value + c.value + d.value)
-
-    watch(numbers, newVal => {
-      console.log(`a: ${newVal.a}, b: ${newVal.b}`)
-    },{
-      immediate: true
-    })
-
-    watch(count, (newVal,oldVal) => {
-      console.log(`count: ${newVal}`)
-      // Work only in ref
-      console.log(`New Value: ${newVal}, Old Value: ${oldVal}`)
-    })
-
-    watch([c, d], ([newC, newD],[oldC, oldD]) => {
-      if(newC !== oldC)
-      history.value.push(`New Value c: ${newC}, Old Value c: ${oldC}`)
-
-      if(newD !== oldD)
-      history.value.push(`New Value d: ${newD}, Old Value d: ${oldD}`)
-    })
-
-    watchEffect(() => {
-      console.log(`From watchEffect a: ${numbers.a}, b: ${numbers.b}`)
-    })
-
-    return {
-      msg,
-      count,
-      numbers,
-      increment,
-      increase,
-      total,
-      c,
-      d,
-      history
-    }
+    return useNumbers()
   }
 }
 </script>
