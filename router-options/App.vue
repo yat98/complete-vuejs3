5115005ba3ca990c9aef7656010960f035e1e3ec
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-12-04 21:01:32 +0800

    Options api refactor

---
+++ router-options/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <RouterLink to="/">Home</RouterLink>
+  <br>
+  <RouterLink to="/posts">Posts</RouterLink>
+  <RouterView />
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
