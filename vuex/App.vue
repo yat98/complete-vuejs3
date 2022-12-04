7211a075fb6fbe60426af2279e485e47cf13bba0
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-12-04 21:28:38 +0800

    Create a vuex store and state

---
+++ vuex/App.vue
@@ -0,0 +1,23 @@
+<template>
+  {{ store.state.count }}
+</template>
+
+<script>
+import { useStore } from 'vuex'
+
+export default {
+  setup() {
+    const store = useStore()
+
+    console.log(store)
+
+    return {
+      store
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
