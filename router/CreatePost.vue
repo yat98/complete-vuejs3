2c82a42a44706d6a16558957227beaaef5f65f5b
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-12-04 17:02:31 +0800

    New post form

--- router/CreatePost.vue
+++ router/CreatePost.vue
@@ -1,13 +1,35 @@
 <template>
   <h3>Create Post</h3>
+  <form @submit.prevent="submit">
+    <input v-model="newPost.title" placeholder="Title" />
+    <br />
+    <textarea cols="50" rows="10" v-model="newPost.content"></textarea>
+    <br />
+    <button>Submit</button>
+  </form>
 </template>
 
 <script>
-export default {
+import { reactive } from "vue";
 
-}
+export default {
+  setup() {
+    const newPost = reactive({
+      title: "",
+      content: "",
+    });
+
+    const submit = () => {
+
+    }
+
+    return {
+      newPost,
+      submit
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
