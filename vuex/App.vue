699416a3253af46a32c7cb69228852d66500aa49
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-12-05 18:41:06 +0800

    More on mutations

--- vuex/App.vue
+++ vuex/App.vue
@@ -1,22 +1,36 @@
 <template>
-  {{ store.state.count }}
-  <button @click="click">Increment</button>
+  <button
+    v-for="post in posts" 
+    :key="post.id"
+    @click="click(post)"
+  >
+    {{ post.title }}  
+  </button>
+  {{ postId }}
 </template>
 
 <script>
+import { computed } from 'vue'
 import { useStore } from 'vuex'
 
 export default {
   setup() {
     const store = useStore()
 
-    console.log(store)
+    const posts = [
+      { id: 1, title: 'Post #1' },
+      { id: 2, title: 'Post #2' },
+    ]
 
-    const click = () => store.commit('increment',{  number: 10 })
+    const click = (post) => {
+      store.commit('setPostId', post.id)
+    }
 
     return {
       store,
-      click
+      posts,
+      postId: computed(() => store.state.postId),
+      click,
     }
   }
 }
--- vuex/store.js
+++ vuex/store.js
@@ -3,13 +3,17 @@ import { createStore } from "vuex"
 export const store = createStore({
   state() {
     return {
-      count: 0
+      count: 0,
+      postId: null,
     }
   },
 
   mutations: {
     increment(state,{ number }){
       state.count += number
+    },
+    setPostId(state,postId){
+      state.postId = postId
     }
   }
 })
