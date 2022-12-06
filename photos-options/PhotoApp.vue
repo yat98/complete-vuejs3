39df5e05c3d1ba5dd0eaa64e0903d1a06ece5f8c
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-12-06 16:44:45 +0800

    Options api refactor

---
+++ photos-options/PhotoApp.vue
@@ -0,0 +1,50 @@
+<template>
+  <Layout>
+    <template #header>
+      Header
+    </template>
+
+    <template #sidebar>
+      <Album
+        v-for="album in albums"
+        :key="album.id"
+        :album="album"
+      />
+    </template>
+
+    <template #content>
+      <RouterView />
+    </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from './Layout.vue';
+import Album from './Album.vue';
+
+export default {
+  components: {
+    Layout,
+    Album,
+  },
+
+  computed: {
+    albums() {
+      return this.$store.state.albums.all
+    }
+  },
+
+  mounted() {
+    this.$store.dispatch('albums/fetch') 
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+body{
+  margin: 0;
+}
+</style>
---
+++ photos-options/PhotoView.vue
@@ -0,0 +1,25 @@
+<template>
+  <img 
+    v-for="photo in photos"
+    :key="photo.id"
+    :src="photo.thumbnailUrl"
+  />
+</template>
+
+<script>
+export default {
+  computed: {
+    photos() {
+      const id = this.$route.params.id
+      if(!id) return
+      this.$store.dispatch("photos/getByAlbum", { albumId: id })
+
+      return this.$store.state.photos.all
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
