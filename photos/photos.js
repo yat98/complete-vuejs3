6ae6f82954127a92af387d8cadb0e60e57147459
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-12-06 15:47:03 +0800

    Vuex level caching for better performance

--- photos/photos.js
+++ photos/photos.js
@@ -3,21 +3,28 @@ export const photos = {
 
   state() {
     return {
-      all: []
+      all: [],
+      cache: {},
     }
   },
 
   mutations: {
-    setPhotos(state, photos) {
+    setPhotos(state, { photos, albumId}) {
       state.all = photos
+      state.cache[albumId] = photos
     }
   },
 
   actions: {
     async getByAlbum(ctx, { albumId }) {
+      if(ctx.state.cache[albumId]){
+        ctx.commit('setPhotos', { photos: ctx.state.cache[albumId], albumId })
+        return
+      }
+
       const result = await window.fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
       const json = await result.json()
-      ctx.commit('setPhotos', json)
+      ctx.commit('setPhotos', { photos: json, albumId })
     }
   },
 }
